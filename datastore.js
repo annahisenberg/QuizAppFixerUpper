9befc3618cd0814e9713cc0e572d21cad66ec5dd
Annah Isenberg <annahisenberg@gmail.com>
2018-05-01 09:56:38 -0400

    )

--- datastore.js
+++ datastore.js
@@ -22,42 +22,6 @@ const STORE = [{
     icon: 'https://i.imgur.com/WAFiAY2.jpg',
     alt: 'Chip and Joanna with family'
   },
-  // {
-  //   question: 'Where was Joanna born?',
-  //   answers: [
-  //     'Kansas',
-  //     'Kentucky',
-  //     'Arkansas',
-  //     'Texas'
-  //   ],
-  //   correctAnswer: 'Kansas',
-  //   icon: 'https://i.imgur.com/prEvLvo.jpg',
-  //   alt: 'A single golden ear of wheat waving in the wind against a bright sky'
-  // },
-  // {
-  //   question: 'Where was Chip born?',
-  //   answers: [
-  //     'Oklahoma',
-  //     'Missouri',
-  //     'New Mexico',
-  //     'Mississippi'
-  //   ],
-  //   correctAnswer: 'New Mexico',
-  //   icon: 'https://i.imgur.com/uZqRw3G.jpg',
-  //   alt: 'Cacti'
-  // },
-  {
-    question: 'What university did Chip and Joanna both attend?',
-    answers: [
-      'Texas A&M',
-      'Baylor',
-      'UT Austin',
-      'Texas State University'
-    ],
-    correctAnswer: 'Baylor',
-    icon: 'https://i.imgur.com/vFVAFD3.png',
-    alt: 'Baylor University main building'
-  },
   {
     question: "What was the name of Chip and Joanna's first store?",
     answers: [
@@ -71,18 +35,6 @@ const STORE = [{
     alt: 'Magnolia store'
   },
   {
-    question: 'When did Chip flip his first house?',
-    answers: [
-      'During high school',
-      'During college',
-      'In his mid-twenties',
-      'On the first season of Fixer Upper'
-    ],
-    correctAnswer: 'During college',
-    icon: 'https://i.imgur.com/nVC4mwy.jpg',
-    alt: 'Old house that needs to be fixed up'
-  },
-  {
     question: 'What is Joanna\'s favorite way to decorate walls?',
     answers: [
       'Shiplap',
@@ -93,18 +45,6 @@ const STORE = [{
     correctAnswer: 'Shiplap',
     icon: 'https://i.imgur.com/3ljnqcP.jpg',
     alt: 'Little sitting area with white shiplap on walls'
-  },
-  {
-    question: 'How did they get their own show?',
-    answers: [
-      'They just applied on HGTV\'s website - duh!',
-      'HGTV approached them after they saw pictures of a recent flip house Chip and Joanna did',
-      'They had a friend working at HGTV who referred them',
-      'Joanna\'s mom called HGTV and told them about Chip and Joanna'
-    ],
-    correctAnswer: 'HGTV approached them after they saw pictures of a recent flip house Chip and Joanna did',
-    icon: 'https://i.imgur.com/FBCo17g.jpg',
-    alt: 'Chip and Joanna posing for Fixer Upper'
   },
   {
     question: 'What style do they use most when decorating the houses?',
--- index.js
+++ index.js
@@ -1,83 +1,112 @@
-//Click on start button to generate quiz questions
-
-
-//initiate quiz
-
 jQuery(function ($) {
-  //declaring variables
+
+  //declaring global variables
   let displayAnswers = [];
   let score = 0;
   let scoreDisplay = $('.score');
   let currentQuestion = 0;
-  let questionDisplay = $('.questionNumber');
+  let questionNumberDisplay = $('.questionNumber');
   let answers;
 
-  //Hiding the popup container and the question and score keeper
-  $('.col-6').hide();
-  $('.popup').hide();
 
 
-  //Starting function startQuiz
+  //Hide the feedback container, the question and score keeper, and the end quiz container
+  function hideSections() {
+    $('.col-6').hide();
+    $('.feedback').hide();
+    $('.end-quiz').hide();
+  }
+
+  //Start function startQuiz
   function startQuiz() {
-    questionDisplay.text(1);
+    questionNumberDisplay.text(1);
     //Clicking on the start button does this
     $('.btn-start').on('click', function () {
       currentQuestion = 0;
 
-      //Calls this function to display the questions and answers
+      //Calls this function to get the questions (and display them) and answers (but do not display them yet)
       displayQuestionsAndAnswers();
 
-      //Appending the answer array in the empty "ul" under the question
+      //Appending the answer array in the empty "ul" under the question and displaying the answers
       $('.answerList').append(displayAnswers);
 
-      //Looping through the answers which we declared in the "displayQuestionsAndAnswers" function
-      for (let i = 0; i < answers.length; i++) {
+      //Call this function below to set click listeners on the answers
+      answersClickListener();
+    });
+  }
 
-        // Adds click listener to the "answerList" ul after loading li's
-        $('.answerList').on('click', ` #${i}`, function (event) {
+  //Define the 'next' button click listener here
+  //Inside the function handle the last screen part
+  //When clicking on the feedback's "next" button,
+  function nextButtonClickListener() {
+    $('.feedback button').on('click', function () {
+      //Hide the feedback box
+      $('.feedback').hide();
 
-          //if the clicked answer's text = the current question's correct answer,
-          if ($(event.target).text() === STORE[currentQuestion].correctAnswer) {
-            //increment the score
-            score++;
-            //Display the score
-            scoreDisplay.text(score);
-            //Hide the .questionAnswerForm
-            $('.questionAnswerForm').hide(); // style="display: none"
-            //Show the popup with the picture
-            $('.popup').show();
-            $('.fixerPhoto').attr('src', STORE[currentQuestion].icon);
-
-            //When clicking on the popup's "next" button,
-            $('.popup button').on('click', function () {
-              //Hide the popup
-              $('.popup').hide();
-              //Call the displayQuestionsAndAnswers function again
-              displayQuestionsAndAnswers();
-              //Show the .questionAnswerForm
-              $('.questionAnswerForm').show();
-              //Display the new answers on the .answerList ul
-              $('.answerList').html(displayAnswers);
-            });
-
-            //Increment the question
-            currentQuestion += 1;
-            questionDisplay.text(currentQuestion);
-            return
-          }
-
-          //If the answer clicked is the wrong answer, increment the current question
-          currentQuestion += 1;
-          questionDisplay.text(currentQuestion);
-          //Empty the answerList ul
-          $('.answerList').empty();
-          //Call the displayQuestionsAndAnswers function
-          displayQuestionsAndAnswers();
-          //Append the new answers display to the answerList ul
-          $('.answerList').append(displayAnswers);
-        });
+      if (currentQuestion >= STORE.length - 1) {
+        if (score >= 3) {
+          $('.final-score').text(`${score}/${STORE.length}`);
+          $('.end-quiz').show();
+        } else {
+          $('.end-quiz').show();
+          $('.end-quiz h2').text(`Aww, looks like you need to watch more Fixer Upper! Your score is ${score}/${STORE.length}`);
+        }
+      } else {
+        currentQuestion += 1;
+        questionNumberDisplay.text(currentQuestion + 1);
+        //Call the displayQuestionsAndAnswers function again
+        displayQuestionsAndAnswers();
+        //Show the .questionAnswerForm
+        $('.questionAnswerForm').show();
+        //Display the new answers on the .answerList ul
+        $('.answerList').html(displayAnswers);
       }
     });
+  }
+
+  function startOver() {
+    $('.end-quiz button').on('click', function () {
+      location.reload();
+    });
+  }
+
+
+  function answersClickListener() {
+    for (let i = 0; i < answers.length; i++) {
+
+      // Adds click listener to the "answerList" ul after loading li's
+      $('.answerList').on('click', ` #${i}`, function (event) {
+
+        //if the clicked answer's text = the current question's correct answer,
+        if ($(event.target).text() === STORE[currentQuestion].correctAnswer) {
+          //increment the score
+          score++;
+          //Display the score
+          scoreDisplay.text(score);
+          //Hide the .questionAnswerForm
+          $('.questionAnswerForm').hide();
+          //Show the feedback with the picture
+          $('.feedback').show();
+          $('.fixerPhoto').attr('src', STORE[currentQuestion].icon);
+
+          return;
+        }
+
+        //If the answer clicked is the wrong answer, increment the current question
+        //Empty the answerList ul
+        $('.answerList').empty();
+        $('.questionAnswerForm').hide();
+        $('.feedback').show();
+        $('.feedback h2').text(`Sorry! The correct answer is "${STORE[currentQuestion].correctAnswer}"`);
+
+
+
+        //Call the displayQuestionsAndAnswers function
+        displayQuestionsAndAnswers();
+        //Append the new answers display to the answerList ul
+        $('.answerList').append(displayAnswers);
+      });
+    }
   }
 
 
@@ -106,8 +135,21 @@ jQuery(function ($) {
   }
 
 
+  function init() {
+    hideSections();
+    nextButtonClickListener();
+    startQuiz();
+    startOver();
+  }
 
-  startQuiz();
 
+  //Call this function to start the app
+  init();
 
 });
+
+
+//refactoring:
+//1. DRY
+//2. Pure functions 
+//3. Look at function names
